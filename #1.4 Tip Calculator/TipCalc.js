5366ce588420aa8b61fe84de4574c1eafc5dcff5
AARUMUGAPANDI <123652671+aarumugapandi400267@users.noreply.github.com>
2024-01-20 12:08:40 +0530

    Add files via upload

--- #1.4 Tip Calculator/TipCalc.js
+++ #1.4 Tip Calculator/TipCalc.js
@@ -2,5 +2,12 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 var Bill = parseInt(prompt("Actual Bill..."));
 
-(Bill > 50 && Bill < 300) ? console.log("Ok") : console.log("Poda");
+function tipCalc(Bill){
+    let tip
+    (Bill > 50 && Bill < 300)?tip=Bill*0.15:tip=Bill*0.2
+    return tip
+}
 
+var Total=parseFloat(tipCalc(Bill))+Bill
+
+console.log("Total Bill..."+Total);
